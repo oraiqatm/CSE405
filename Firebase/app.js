2d41b29e39f9e30c5392ce964dd211aab7e6cfc2
oraiqatm <004782735@coyote.csusb.edu>
2020-05-01 02:41:16 -0700

    add delete must refresh'

--- Firebase/app.js
+++ Firebase/app.js
@@ -1,27 +1,48 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
+
 
 //Create element and render cafe
 function renderCafe(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
+    let cross = document.createElement('div');
 
     li.setAttribute('data-id', doc.id)
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent = "x";
 
     li.appendChild(name);
     li.appendChild(city);
     cafeList.appendChild(li);
+    li.appendChild(cross);
+
+    //deleting data
+    cross.addEventListener('click', (e) =>{
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    })
 
 }
 
-
+//Getting data
 db.collection('cafes').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         renderCafe(doc);
     })
 })
 
+//Saving data
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
 
-    
+})    
