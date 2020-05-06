cbd92cad3bace625504bbb88705e615f5e8086a4
oraiqatm <004782735@coyote.csusb.edu>
2020-05-05 20:42:37 -0700

    firebase-auth

---
+++ Firebase_Auth/scripts/auth.js
@@ -0,0 +1,82 @@
+//listen for auth status changes
+auth.onAuthStateChanged(user =>{
+    if(user){
+        //get data
+        db.collection('guides').onSnapshot(snapshot =>{
+            setupGuides(snapshot.docs);
+            setupUI(user);
+        }, err=>{
+            console.log(err, message);
+        });
+    }else {
+        setupUI();
+        setupGuides([]);
+        
+    }
+})
+
+
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content'].value
+    }).then(() =>{
+        //close modal and reset form
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        createForm.reset();
+    }).catch(err => {
+        console.log(err, message);
+    })
+});
+
+
+//signup
+const signupForm = document.querySelector('#signup-form');
+
+signupForm.addEventListener('submit', (e) =>{
+    //prevents Default action
+    e.preventDefault();
+
+    //get user infor 
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    //sign up the user 
+    auth.createUserWithEmailAndPassword(email, password).then(cred =>{
+        return db.collection('users').doc(cred.user.uid).set({
+            bio: signupForm['signup-bio'].value
+        });
+        
+    }).then(() => {
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+
+});
+
+//logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) =>{
+    e.preventDefault();
+    auth.signOut();
+})
+
+//sign in 
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred =>{
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    })
+})
